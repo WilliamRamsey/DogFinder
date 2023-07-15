31707d6b95453337ab8516c58dbd31f906aac46c
WilliamRamsey <williamdawsonramsey@gmail.com>
2023-07-15 19:36:21 -0400

    Create animal form finished

--- API/api.py
+++ API/api.py
@@ -1,21 +1,44 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 from animal import *
+from person import *
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    file = open("Client/index.html", "r")
+def get_page(location):
+    file = open(location, "r")
     page = file.read()
     file.close()
     return page
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/index.html')
+def index():
+    page = get_page("Client/index.html")
+    return page
+
+# Opens Case Page to receive data about animal
+@app.route('/new_case.html')
+def new_case():
+    lost = request.args.get('lost')
+    print(lost)
+    page = get_page("Client/new_case.html")
+    return page
+
+# Receives information from new_case.html creates new animal and animal id and saves to database
+@app.route('/create_new_case.html', methods=['POST', 'GET'])
+def create_new_case():
+    lost = request.args.get()
+    print(lost)
+    return lost
+
 
 @app.route("/animal_porfile/<animal_id>")
 def animal_porfile(animal_id):
     file = open("Client/animal_profile.html", "r")
     page = file.read()
     page = page.format(id = animal_id)
-    print(page)
     file.close()
     return page
 
